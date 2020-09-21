50b6e2204b090f4b5c142a04e2f27317665b6127
IshakPasolle <69841614+Ishak04@users.noreply.github.com>
2020-09-21 21:23:12 +0800

    Add files via upload

---
+++ TrojanNomor.py
@@ -0,0 +1,19 @@
+import os
+import sys
+import time
+
+os.system('clear')
+os.system('figlet trojan virus')
+print(* *)
+target = input('\x1b[92mmasukkan no target' : \x1b[91m')
+jumlah = input('\x1b[92mmasukkan jumlah virus' : \x1b[91m' )
+
+if  lean(target) < 12:
+    print ('\x1b[92mMasukkan nomor dengan benar ya omx1b[91m')
+    print('\x1b[97m---< Cyber security official <---\x1b[97m!!')
+    exit()
+   for i in range(100000000000000000):
+               print (f "\x1b[91mvirus berhasil dikirim awokawok\x1b[92m"):
+               print('x\1b[97m---< Cyber security official <---\x1b[97m'):
+              time.sleep(0.7)
+    
